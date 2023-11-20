999f80405d44f9c681d23a6aa49319e87e9f245f
Martyna Michalska <m.michalska4126@gmail.com>
2023-11-20 22:40:28 +0100

    feat: extract outbutton component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,33 @@
+import React, {forwardRef} from 'react';
+
+import style from './Button.module.scss';
+
+const buttonVariants = {
+  outlined: style.button_outlined,
+  'solid-blue': style.button_solid_blue,
+  'solid-gray': style.button_solid_gray,
+};
+
+type ButtonVariant = keyof typeof buttonVariants;
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: ButtonVariant;
+}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({variant = 'outlined', className, children, ...props}: ButtonProps, ref) => {
+    return (
+      <button
+        className={`${buttonVariants[variant]} ${className}`}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+Button.displayName = 'Button';
+
+export default Button;
