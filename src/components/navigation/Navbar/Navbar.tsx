0502a07910cd53dc1fe95a5250d943de7e6b765f
Martyna Michalska <m.michalska4126@gmail.com>
2023-11-25 16:03:41 +0100

    feat: responsive ui update

--- src/components/navigation/Navbar/Navbar.tsx
+++ src/components/navigation/Navbar/Navbar.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {Button} from '@/components/UI';
+import {useWindowSize} from 'react-use';
+import {IoMenu} from 'react-icons/io5';
 
 import style from './Navbar.module.scss';
 
 import startupzLogo from '/img/startupz_logo.svg';
 
 const Navbar = () => {
-  return (
+  const {width} = useWindowSize();
+  return width > 768 ? (
     <div className={style.navbar}>
       <img src={startupzLogo} />
       <div className={style.navbar__links_wrapper}>
@@ -15,6 +18,15 @@ const Navbar = () => {
         <div>
           <Button variant="outlined">Work with us!</Button>
         </div>
+      </div>
+    </div>
+  ) : (
+    <div className={style.navbar}>
+      <img height="30px" src={startupzLogo} />
+      <div>
+        <button className={style.navbar__mobile_menu_control}>
+          <IoMenu size="30" />
+        </button>
       </div>
     </div>
   );
