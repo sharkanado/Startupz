0d129e140e98c7493df132959e2926002346e424
Martyna Michalska <m.michalska4126@gmail.com>
2023-11-26 20:40:35 +0100

    feat: handle message sending

--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -1,7 +1,5 @@
 import Button from './Button/Button';
 import SectionHeader from './SectionHeader/SectionHeader';
 import BulletPoint from './BulletPoint/BulletPoint';
-import Input from './Input/Input';
-import TextArea from './TextArea/TextArea';
 
-export {Button, SectionHeader, BulletPoint, Input, TextArea};
+export {Button, SectionHeader, BulletPoint};
--- src/components/sections/ContactForm/ContactForm.tsx
+++ src/components/sections/ContactForm/ContactForm.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
-import {Button, Input, TextArea} from '@/components/common';
+import React, {useState} from 'react';
+import toast from 'react-hot-toast';
+import {Button} from '@/components/common';
 
 import style from './ContactForm.module.scss';
 
 import rocket from '/img/rocket.svg';
 
 const ContactForm = () => {
+  const [message, setMessage] = useState<string>('');
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+
+  const handleSendMessage = () => {
+    if (message && name && email) {
+      console.log(
+        `${name} with email address ${email} sent a message: ${message}`
+      );
+      //TODO: api call & error handling
+      clearState();
+      toast.success('Message sent!');
+    } else toast.error('Please fill all fields available!');
+  };
+
+  const clearState = () => {
+    setEmail('');
+    setName('');
+    setMessage('');
+  };
+
   return (
     <div className={style.contact_form}>
       <div className={style.contact_form__inner_wrapper}>
@@ -15,11 +37,29 @@ const ContactForm = () => {
         </h3>
         <p>Share your excitement with us</p>
         <div className={style.contact_form__form}>
-          <Input placeholder="Name" />
-          <Input placeholder="Email" />
-          <TextArea placeholder="Message" />
+          <input
+            value={name}
+            className={style.contact_form__input}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Name"
+          />
+
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className={style.contact_form__input}
+            placeholder="Email"
+          />
+          <textarea
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            className={style.contact_form__textarea}
+            placeholder="Message"
+          />
           <div>
-            <Button variant="solid-gray">Shoot us a message</Button>
+            <Button onClick={() => handleSendMessage()} variant="solid-gray">
+              Shoot us a message
+            </Button>
           </div>
         </div>
       </div>
