63b082766e0cb1aaa5089789cc7929fb96320165
Martyna Michalska <m.michalska4126@gmail.com>
2023-11-27 13:26:21 +0100

    fix: scroll position persisting

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import {Home} from './pages';
 import Hiring from './pages/Hiring';
+import {useEffect} from 'react';
+import {useLocation} from 'react-router-dom';
 
 const applicationRoutes = [
   {
@@ -15,12 +17,19 @@ const applicationRoutes = [
     component: <Hiring />,
   },
 ];
-const App = () => (
-  <Routes>
-    {applicationRoutes.map((route, idx) => (
-      <Route key={idx} path={route.path} element={route.component} />
-    ))}
-  </Routes>
-);
+const App = () => {
+  const {pathname} = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return (
+    <Routes>
+      {applicationRoutes.map((route, idx) => (
+        <Route key={idx} path={route.path} element={route.component} />
+      ))}
+    </Routes>
+  );
+};
 
 export default App;
